cc4c84b50385ebf8ce34998e003baee3e6090a27
Naggi <nagase2@gmail.com>
2015-04-08 16:06:01 +0900

    di

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -15,7 +15,7 @@ public class App
 {
 	@RequestMapping("/") //左のパスでアクセスが有った時に、このメソッドが呼ばれる
 	String home(){
-		return "Hellっっｋo ssssssdxxxxdっっｘdoos!";
+		return "Hellっっｋo ssssssdxっっｘdoos!";
 	}
 	@RequestMapping("/nagase")
 	String myHome(){
