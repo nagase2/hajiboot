c030c16170d0021f5b1f9bfbd0f62ee151e614cd
Naggi <nagase2@gmail.com>
2015-04-08 13:49:25 +0900

    Error has been found during testing for bug fix1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -15,7 +15,7 @@ public class App
 {
 	@RequestMapping("/") //左のパスでアクセスが有った時に、このメソッドが呼ばれる
 	String home(){
-		return "Hellっっｋo sssddっっｘdoos!";
+		return "Hellっっｋo ssssssddっっｘdoos!";
 	}
 	@RequestMapping("/nagase")
 	String myHome(){
